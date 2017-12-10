fcffd9494ea11dda41bf9ebb205a40bb817e8762
Hellowor1d <1034597776@qq.com>
2017-12-11 00:12:55 +0800

    添加路由

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <h2>跳转页面</h2>
+    <ul>
+      <li><router-link to="/schedule">schedule</router-link></li>
+      <li><router-link to="/classroom">classroom</router-link></li>
+      <li><router-link to="/reset">reset</router-link></li>
+      <li><router-link to="/">index</router-link></li>
+    </ul>
   </div>
 </template>
 
@@ -14,7 +21,7 @@ export default class App extends Vue {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +29,20 @@ export default class App extends Vue {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
 }
 </style>
