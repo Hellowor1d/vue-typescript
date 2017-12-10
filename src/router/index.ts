558249ad9dedcea57cab1884ebaefe5fb63ede62
Hellowor1d <1034597776@qq.com>
2017-12-11 00:14:20 +0800

    引入子组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
+import Login from '@/components/Login.vue';
+import Schedule from '@/components/Schedule.vue';
+import Classroom from '@/components/Classroom.vue';
+import Reset from '@/components/Reset.vue';
 
 Vue.use(Router);
 
@@ -10,6 +14,26 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/schedule',
+      name: 'schedule',
+      component: Schedule,
+    },
+    {
+      path: '/classroom',
+      name: 'classroom',
+      component: Classroom,
+    },
+    {
+      path: '/reset',
+      name: 'reset',
+      component: Reset,
     },
   ],
 });
