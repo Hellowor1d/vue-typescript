fa83d1749ce32c02e1d6af3f9734da128ae8ba68
Hellowor1d <1034597776@qq.com>
2017-12-14 17:54:48 +0800

    去除分号，逗号报错，增加开发环境 console支持

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,10 @@ module.exports = {
     },
     // add your custom rules here
     rules: {
+      /* "off" 或 0 - 关闭规则
+      *  "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+      *  "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+      */
         // don't require .vue extension when importing
         'import/extensions': [
             'error',
@@ -30,12 +34,19 @@ module.exports = {
                 vue: 'never'
             }
         ],
+        //comma
+        'comma-dangle': [0, 'never'],
+        //semicolon
+        'semi': 0,
         // allow optionalDependencies
         'import/no-extraneous-dependencies': [
             'error', {
                 optionalDependencies: ['test/unit/index.js']
             }
         ],
+        'no-console': process.env.NODE_ENV === 'production'
+        ? 'error'
+        : 'off',
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production'
             ? 'error'
