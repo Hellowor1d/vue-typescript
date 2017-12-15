324d93403bd813fd3488b60a09a7b90d02ce5f16
Hellowor1d <1034597776@qq.com>
2017-12-15 18:07:29 +0800

    添加 vuex

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App.vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import router from './router/index';
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router/index'
 import i18n from './locale/i18n'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+Vue.use(ElementUI,Vuex);
 
 /* eslint-disable no-new */
 const app = new Vue({
